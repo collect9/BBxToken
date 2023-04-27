012ade585916e4a216f18bd58bbab1cb4e4ebefc
collect9 <admin@collectix.io>
2023-04-27 11:55:30 -0700

    owner control update

--- C9OwnerControl.sol
+++ C9OwnerControl.sol
@@ -26,6 +26,8 @@ import "./abstract/C9Errors.sol";
 */
 
 abstract contract C9OwnerControl is AccessControl, ERC2771Context {
+    bytes32 public constant DEFAULT_OWNER_ROLE = keccak256("DEFAULT_OWNER_ROLE");
+    
     address public owner;
     address public pendingOwner;
     bool private _frozen;
@@ -36,8 +38,9 @@ abstract contract C9OwnerControl is AccessControl, ERC2771Context {
     );
 
     constructor() {
-        owner = _msgSender();
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _grantRole(DEFAULT_OWNER_ROLE, _msgSender());
+        owner = _msgSender();
     }
 
     /*
@@ -71,6 +74,9 @@ abstract contract C9OwnerControl is AccessControl, ERC2771Context {
         _;
     }
 
+    /*
+     * @dev Required override.
+     */ 
     function _msgSender()
     internal view
     override(ERC2771Context, Context)
@@ -78,6 +84,9 @@ abstract contract C9OwnerControl is AccessControl, ERC2771Context {
         return super._msgSender();
     }
 
+    /*
+     * @dev Required override.
+     */ 
     function _msgData()
     internal view
     override(ERC2771Context, Context)
@@ -86,72 +95,77 @@ abstract contract C9OwnerControl is AccessControl, ERC2771Context {
     }
 
     /**
-     * @dev It will not be possible to call `onlyRole(DEFAULT_ADMIN_ROLE)` 
-     * functions anymore, unless there are other accounts with that role.
-     *
-     * NOTE: If the renouncer is the original contract owner, the contract 
-     * is left without an owner.
+     * @dev Allows account to renounce current role.
+     * @param role The role to renounce.
+     * @param account The account to renounce role of.
+     * @notice If the renouncer is the original contract owner, the contract 
+     * is effectively left without an admin/owner.
      */
     function renounceRole(bytes32 role, address account)
-        public override {
-            if (account != _msgSender()) revert C9Unauthorized();
-            if (!hasRole(role, account)) revert NoRoleOnAccount();
-            _revokeRole(role, account);
+    public override {
+        if (account != _msgSender()) revert C9Unauthorized();
+        if (!hasRole(role, account)) revert NoRoleOnAccount();
+        _revokeRole(role, account);
     }
 
     /**
      * @dev Override that makes it impossible for other admins 
-     * to revoke the admin rights of the original contract deployer.
-     * As a result admin also cannot revoke itself either.
-     * But it can still renounce.
+     * to revoke the admin rights of the contract owner.
+     * As a result admin also cannot revoke itself either 
+     * but it can still renounce.
+     * @param role The role to renounce.
+     * @param account The account to renounce role of.
      */
     function revokeRole(bytes32 role, address account)
-        public override
-        onlyRole(DEFAULT_ADMIN_ROLE) {
-            if (account == owner) revert C9Unauthorized();
-            if (!hasRole(role, account)) revert NoRoleOnAccount();
-            _revokeRole(role, account);
+    public override
+    onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (account == owner) revert C9Unauthorized();
+        if (!hasRole(role, account)) revert NoRoleOnAccount();
+        _revokeRole(role, account);
     }
 
     /**
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Internal function without access restriction. This is meant to make AccessControl 
-     * functionally equivalent to 2-step Ownable.
+     * This is meant to make AccessControl functionally equivalent 
+     * to 2-step Ownable.
+     * @param newOwner The new owner of the contract.
+     * @notice Old owner still has admin role. That needs to either be renounced 
+     * or revoked by the new owner.
+     * @notice Emits an ownership transfer event.
      */
-    function _transferOwnership(address _newOwner)
-        private {
-            delete pendingOwner;
-            address _oldOwner = owner;
-            owner = _newOwner;
-            _grantRole(DEFAULT_ADMIN_ROLE, _newOwner);
-            _revokeRole(DEFAULT_ADMIN_ROLE, _oldOwner);
-            emit OwnershipTransfer(_oldOwner, _newOwner);
+    function _transferOwnership(address newOwner)
+    private {
+        delete pendingOwner;
+        address _oldOwner = owner;
+        owner = newOwner;
+        // 1. Grant owner and admin roles to new owner
+        _grantRole(DEFAULT_OWNER_ROLE, newOwner);
+        _grantRole(DEFAULT_ADMIN_ROLE, newOwner);
+        // 2. Remove owner role from old owner
+        _revokeRole(DEFAULT_OWNER_ROLE, _oldOwner);
+        emit OwnershipTransfer(_oldOwner, newOwner);
     }
 
     /**
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner. This is meant to make AccessControl 
-     * functionally equivalent to Ownable.
+     * This can only be called by the current owner.
+     * @param newOwner The new owner of the contract.
      */
-    function transferOwnership(address _newOwner)
+    function transferOwnership(address newOwner)
     external
-    onlyRole(DEFAULT_ADMIN_ROLE)
-    validTo(_newOwner)
-    notFrozen() {
-        pendingOwner = _newOwner;
+    onlyRole(DEFAULT_OWNER_ROLE)
+    validTo(newOwner) {
+        pendingOwner = newOwner;
     }
 
     /**
-     * @dev The new owner accepts the ownership transfer. The original owner will
-     * still need to renounceRole DEFAULT_ADMIN_ROLE to fully complete 
-     * this process, unless original owner wishes to remain in that role.
+     * @dev The new owner accepts the ownership transfer.
      */
     function acceptOwnership()
-        external
-        notFrozen() {
-            if (pendingOwner != _msgSender()) revert C9Unauthorized();
-            if (pendingOwner == address(0)) revert NoTransferPending();
-            _transferOwnership(pendingOwner);
+    external {
+        if (pendingOwner != _msgSender()) revert C9Unauthorized();
+        if (pendingOwner == address(0)) revert NoTransferPending();
+        _transferOwnership(pendingOwner);
     }
 
     /**
@@ -159,42 +173,50 @@ abstract contract C9OwnerControl is AccessControl, ERC2771Context {
      * pending owner do this as well, we're keeping it ADMIN only.
      */
     function cancelTransferOwnership()
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE) {
-            if (pendingOwner == address(0)) revert NoTransferPending();
-            delete pendingOwner;
+    external
+    onlyRole(DEFAULT_OWNER_ROLE) {
+        if (pendingOwner == address(0)) revert NoTransferPending();
+        delete pendingOwner;
     }
 
     /**
      * @dev Set the trusted forwarder of the contract.
+     * @param forwarder The address of the forwarder.
      */
     function setTrustedForwarder(address forwarder)
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE)
-        addressNotSame(_trustedForwarder, forwarder) {
-            _trustedForwarder = forwarder;
+    external
+    onlyRole(DEFAULT_OWNER_ROLE)
+    addressNotSame(_trustedForwarder, forwarder) {
+        _trustedForwarder = forwarder;
     }
 
     /**
-     * @dev Flag that sets global toggle to freeze redemption. 
-     * Users may still cancel redemption and unlock their 
-     * token if in the process.
+     * @dev Flag that sets global toggle to freeze functionality. 
+     * For method that have the freeze modifier.
+     * @param toggle Freeze (true) or unfreeze (false).
      */
-    function toggleFreeze(bool _toggle)
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE) {
-            if (_frozen == _toggle) {
-                revert BoolAlreadySet();
-            }
-            _frozen = _toggle;
+    function toggleFreeze(bool toggle)
+    external
+    onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (_frozen == toggle) {
+            revert BoolAlreadySet();
+        }
+        _frozen = toggle;
     }
 
-    function __destroy(address _receiver, bool confirm)
-        public virtual
-        onlyRole(DEFAULT_ADMIN_ROLE) {
-            if (!confirm) {
-                revert ActionNotConfirmed();
-            }
-    		selfdestruct(payable(_receiver));
+    /**
+     * @dev Allows for contract to be destroyed. The virtual flag
+     * allows this to be overriden and disabled.
+     * @param receiver The address to receive any remaining balance.
+     * @param confirm Confirmation of destruction of the contract.
+     * @notice This will eventually be deprecated and not work.
+     */
+    function __destroy(address receiver, bool confirm)
+    public virtual
+    onlyRole(DEFAULT_OWNER_ROLE) {
+        if (!confirm) {
+            revert ActionNotConfirmed();
         }
+        selfdestruct(payable(receiver));
+    }
 }
